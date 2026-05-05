8fc933905035764c3208084657a1277e31be6a28
syedubaid11 <syedabuubaid@gmail.com>
2026-05-06 00:05:11 +0530

    fix : fixed lenis

--- components/LenisProvider.tsx
+++ components/LenisProvider.tsx
@@ -12,11 +12,6 @@ export default function LenisProvider({
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
